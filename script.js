d2caba8f4abc2071d7391f2c3aeeefb2262e8bf9
Frank Gonzalez <frank.gonzalez628@gmail.com>
2016-08-24 20:10:59 -0500

    first solution done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+var template = document.getElementsByTagName('body')[0];
+
+for (var i = 0; i < 99; i++) {
+    var div = document.createElement('div');
+
+    div.style.float = 'left';
+    div.style.width = '11.1%';
+    div.style.paddingBottom = '11.1%';
+
+    if (i % 2 === 0) {
+        div.style.backgroundColor = 'red';
+    } else {
+        div.style.backgroundColor = 'black';
+    }
+
+    template.appendChild(div);
+}
